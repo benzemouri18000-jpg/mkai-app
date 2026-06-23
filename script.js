c39af05294388ef0f7459a3671190f510eb4bdd2
benzemouri18000-jpg <benzemouri18000@gmail.com>
2026-06-23 11:07:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-
 document.addEventListener("DOMContentLoaded", () => {
-
-  /* =========================
-     SMOOTH SCROLL MENU
-  ========================= */
 
   document.querySelectorAll('a[href^="#"]').forEach(a => {
     a.addEventListener("click", e => {
@@ -15,36 +10,10 @@ document.addEventListener("DOMContentLoaded", () => {
       e.preventDefault();
 
       target.scrollIntoView({
-        behavior: "smooth",
-        block: "start"
+        behavior: "smooth"
       });
 
     });
   });
-
-  /* =========================
-     REVEAL ANIMATION (SCROLL)
-  ========================= */
-
-  const elements = document.querySelectorAll(".reveal");
-
-  const reveal = () => {
-
-    const trigger = window.innerHeight * 0.85;
-
-    elements.forEach(el => {
-
-      const top = el.getBoundingClientRect().top;
-
-      if (top < trigger) {
-        el.classList.add("active");
-      }
-
-    });
-
-  };
-
-  window.addEventListener("scroll", reveal);
-  reveal(); // run once at start
 
 });
